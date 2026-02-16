577ba8d378f36f4e599b79a1ac99289c5b930165
Bruno Martins <brunocorream@gmail.com>
2026-02-16 21:09:31 +0000

    fet: add Post model and session based view counter preotection

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
 def index(request):
     return render(request, 'dashboard/index.html')
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    session_key = f'viewed_post_{post.id}'
+
+    if not request.session.get(session_key):
+        post.views_count += 1
+        post.save()
+
+        request.session[session_key] = True
+        request.session.set_expiry(86400)
+
+    return render(request, 'blog/post_detail.html', {'post': post})
